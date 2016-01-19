7c20c27b782dc318b4917b87700ae2ceda2d7951
2-26 <a2-26@2-26demac-mini.local>
2016-01-19 10:36:24 +0800

    Initial Commit

---
+++ UIImagePickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImagePickerController
+//
+//  Created by 2-26 on 16/1/19.
+//  Copyright © 2016年 Becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImagePickerController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImagePickerController
+//
+//  Created by 2-26 on 16/1/19.
+//  Copyright © 2016年 Becky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
